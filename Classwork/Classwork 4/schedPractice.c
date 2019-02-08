cc75722b0228aa3e0f901a0bc7d4435d2b18f990
Michael Simmons <msimmo19@lion.lmu.edu>
2019-02-07 18:56:59 -0800

    Started

---
+++ Classwork/Classwork 4/schedPractice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+   int main( int argc, char * argv[] ) {
+      
+      char *names[MAX];
+      int *times[MAX];
+      fgets(names, MAX, stdin);
+      fgets(times, MAX, stdin);
+      for (int i = 0; i < times)
+
+   }
