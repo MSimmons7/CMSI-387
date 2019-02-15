e03d22b6ba277314009e6a5f8dcd7532ed0d7964
Michael Simmons <msimmo19@lion.lmu.edu>
2019-02-14 18:46:05 -0800

    Got it to compile

--- Classwork/Classwork 5/bounded.c
+++ Classwork/Classwork 5/bounded.c
@@ -11,8 +11,8 @@
       int n;
       void producer();
       void consumer();
-      int wait( int );
-      int signal( int );
+      int wait_( int );
+      int signal_( int );
       while( 1 ) {
          printf( "\n\n   1.Producer\n   2.Consumer\n   3.Exit" );
          printf("\n      Enter your choice: ");
@@ -38,29 +38,74 @@
       return 0;
    }
     
-   int wait( int s ) {
+   int wait_( int s ) {
       return( --s );
    }
     
-   int signal( int s ) {
+   int signal_( int s ) {
       return( ++s );
    }
     
    void producer() {
-      mutex = wait( mutex );
-      full  = signal( full );
-      empty = wait( empty );
+      mutex = wait_(mutex);
+      full  = signal_( full);
+      empty = wait_(empty);
       x++;
       printf( "\n  Producer produces the item %d", x );
-      mutex = signal( mutex );
+      mutex = signal_(mutex);
    }
     
    void consumer()
    {
-      mutex = wait( mutex );
-      full  = wait( full );
-      empty = signal( empty );
+      mutex = wait_(mutex);
+      full  = wait_(full);
+      empty = signal_(empty);
       printf( "\n  Consumer consumes item %d", x );
       x--;
-      mutex = signal( mutex );
+      mutex = signal_(mutex);
    }
+
+
+
+
+
+  /* public class Simple2Threads {
+public static void main(String args[]){
+Thread childThread = new Thread(new Runnable(){
+public void run(){
+sleep(3000);
+System.out.println("Child is done sleeping 3 seconds.");
+}
+});
+childThread.start();
+sleep(5000);
+System.out.println("Parent is done sleeping 5 seconds.");
+}
+private static void sleep(int milliseconds){
+try{
+Thread.sleep(milliseconds);
+} catch(InterruptedException e){
+// ignore this exception; it won’t happen anyhow
+}
+}
+}
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+static void *child(void *ignored){
+sleep(3);
+printf("Child is done sleeping 3 seconds.\n");
+return NULL;
+}
+int main(int argc, char *argv[]){
+pthread_t child_thread;
+int code;
+code = pthread_create(&child_thread, NULL, child, NULL);
+if(code){
+fprintf(stderr, "pthread_create failed with code %d\n", code);
+}
+sleep(5);
+printf("Parent is done sleeping 5 seconds.\n");
+return 0;
+} */
