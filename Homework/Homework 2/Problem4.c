3123a768d17e56250818e62f085e79d6415443e5
nrajavas <nrajavas@gmail.com>
2019-03-07 12:23:57 -0800

    Started problem 4

---
+++ Homework/Homework 2/Problem4.c
@@ -0,0 +1,31 @@
+public class BoundedBuffer {
+    private Object[] buffer = new Object[20]; // arbitrary size
+    private int numOccupied = 0;
+    private int firstOccupied = 0;
+
+    public synchronized void insert(Object o)
+        throws InterruptedException
+            {
+                while(numOccupied == buffer.length)
+                 // wait for space
+                wait();
+                buffer[(firstOccupied + numOccupied) % buffer.length] = o;
+                numOccupied++;
+                // in case any retrieves are waiting for data, wake them
+                notifyAll();
+            }
+    public synchronized Object retrieve()
+        throws InterruptedException
+        {
+            while(numOccupied == 0)
+            // wait for data
+            wait();
+            Object retrieved = buffer[firstOccupied];
+            buffer[firstOccupied] = null; // may help garbage collector
+            firstOccupied = (firstOccupied + 1) % buffer.length;
+            numOccupied--;
+            // in case any inserts are waiting for space, wake them
+            notifyAll();
+            return retrieved;
+        }
+}
