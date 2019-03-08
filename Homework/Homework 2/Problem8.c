440bc5c2b00ccb29a6efc343411badac7cdaee9b
Michael Simmons <msimmo19@lion.lmu.edu>
2019-03-07 19:40:04 -0800

    Finished 8

--- Homework/Homework 2/Problem8.c
+++ Homework/Homework 2/Problem8.c
@@ -1,13 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]){
-  int arrSize = 1000000;
-  char arr[arrSize];
-  for (int i = 0; i < arrSize; i++) {
-    if (i == 4095) {
-      return arr[i];
+  clock_t start, end;
+  start = clock();
+  char *ptr = malloc(1000000);
+  for (int i = 0; i < 1000000; i++) {
+    ptr[i] = i;
+  }
+  for (int i = 0; i < 1000000; i++) {
+    if ((i+1) % 4096) {
+      printf("");
     }
   }
+  end = clock();
+  double time_taken = ((double)start)/CLOCKS_PER_SEC; // in seconds 
+  printf("took %f seconds to execute \n", time_taken); 
 }
